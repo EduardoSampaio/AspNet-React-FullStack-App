0fa53e0239448b657c78d68f43c34ec70a21691f
Eduardo <edwardviana@gmail.com>
2025-03-18 21:00:27 -0300

    activity card style

--- src/client/src/features/activities/dashboard/ActivityCard.tsx
+++ src/client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,14 +1,17 @@
 import {
+    Avatar,
     Box,
     Button,
     Card,
     CardActions,
     CardContent,
+    CardHeader,
     Chip,
-    Typography,
+    Divider,
 } from "@mui/material";
-import { useActivities } from "../../../lib/hooks/useActivities";
 import { Link } from "react-router";
+import { AccessTime, Place } from "@mui/icons-material";
+import { Typography } from '@mui/material';
 
 type Props = {
     activity: Activity;
@@ -16,25 +19,49 @@ type Props = {
 
 export default function ActivityCard({
     activity,
-}: Props) {
-    const {deleteActivity} = useActivities();
-    
+}: Props) { 
+    const isHost = false;
+    const isGoing = false;
+    const label = isHost ? "You are hosting": "You are going";
+    const isCancelled = false;
+    const color = isHost ? "secondary" : isGoing ? 'warning' : "default";
+
     return (
         <Card sx={{ borderRadius: 3 }}>
-            <CardContent>
-                <Typography variant="h5">{activity.title}</Typography>
-                <Typography sx={{ color: "text.secondary", md: 1 }}>
-                    {activity.date}
-                </Typography>
-                <Typography variant="body2">{activity.description}</Typography>
-                <Typography variant="subtitle1">
-                    {activity.city} / {activity.venue}
-                </Typography>
+            <Box display='flex' alignItems='center' justifyContent='space-between'>
+                <CardHeader avatar={<Avatar sx={{height: 80, width: 80}}/>}
+                title={activity.title}
+                titleprops={{ sx: {fontWeight: "bold", fontSize: 20}}}
+                subheader={
+                    <>
+                        Hosted by{" "} <Link to={`/profiles/bob`}>Bob</Link> 
+                    </>
+                }
+                />
+                <Box display='flex' flexDirection='column' gap={2} mr={2}>
+                    {(isHost || isGoing) && <Chip label={label} color={color} sx={{borderRadius: 2}}/>}
+                    {isCancelled && <Chip label="Cancelled" color="error" sx={{borderRadius: 2}}/>}
+                </Box>
+            </Box>
+
+            <Divider sx={{ mb: 3 }}/>
+
+            <CardContent sx={{ p: 0 }}>
+                <Box display="flex" alignItems="center" mb={2} px={2}>
+                    <AccessTime sx={{ mr: 2 }} />
+                    <Typography variant="body2">{activity.date}</Typography>
+                    <Place sx={{ ml: 2, mr: 1 }} />
+                    <Typography variant="body2">{activity.venue}</Typography>
+                </Box>    
+                <Divider />
+                <Box display="flex" gap={2} sx={{backgroundColor: 'gray.200', py:3, pl: 3}}>
+
+                </Box>
             </CardContent>
             <CardActions
-                sx={{ display: "flex", justifyContent: "space-between", pb: 2 }}
+                sx={{ pb: 2 }}
             >
-                <Chip label={activity.category} variant="outlined"></Chip>
+                <Typography variant="body2">{activity.description}</Typography>
                 <Box display="flex" gap={3}>
                     <Button
                         component={Link}
@@ -42,17 +69,9 @@ export default function ActivityCard({
                         size="medium"
                         variant="contained"
                         onClick={() => {}}
+                        sx={{ display: 'flex', justifySelf: 'self-end', borderRadius: 3 }}
                     >
                         View
-                    </Button>
-                    <Button
-                        size="medium"
-                        variant="contained"
-                        color="error"
-                        disabled={deleteActivity.isPending}
-                        onClick={async () => await deleteActivity.mutateAsync(activity.id)}
-                    >
-                        Delete
                     </Button>
                 </Box>
             </CardActions>
